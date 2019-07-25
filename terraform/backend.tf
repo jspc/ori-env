b86d8fb5f03876f20d6eb5b55582f60cff20e750
jspc <james@zero-internet.org.uk>
2019-07-25 22:44:41 +0100

    Use digital ocean space as backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -26,3 +26,16 @@ provider "aws" {
     s3 = "https://nyc3.digitaloceanspaces.com"
   }
 }
+
+terraform {
+  backend "s3" {
+    skip_requesting_account_id = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms = true
+    skip_metadata_api_check = true
+    endpoint = "https://nyc3.digitaloceanspaces.com"
+    region = "us-east-1"
+    bucket = "state-jspc-pw"
+    key = "ori/env/terraform.tfstate"
+  }
+}
